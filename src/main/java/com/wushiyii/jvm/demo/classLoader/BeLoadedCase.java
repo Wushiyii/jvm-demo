10aaaa104b61c39b6d2a592f680b57ea52c5bec4
wuguoqing <wuguoqing@qudian.com>
2019-09-08 21:03:40 +0800

    被动引用case 2

--- src/main/java/com/wushiyii/jvm/demo/classLoader/BeLoadedCase.java
+++ src/main/java/com/wushiyii/jvm/demo/classLoader/BeLoadedCase.java
@@ -3,7 +3,7 @@ package com.wushiyii.jvm.demo.classLoader;
 public class BeLoadedCase {
 
     /**
-     * 被动引用case 1
+     * 被动引用case
      * 通过子类引用父类静态字段，不会导致子类初始化
      */
     static class SuperClass{
@@ -20,6 +20,8 @@ public class BeLoadedCase {
     }
 
     public static void main(String[] args) {
-        System.out.println(SubClass.val);
+//        System.out.println(SubClass.val); // case 1 :  通过子类引用父类静态字段，不会导致子类初始化
+        SuperClass[] superClasses = new SuperClass[10]; // case 2 : 创建动作由虚拟机调用newarray指令触发，生成的类并不是对象，而是一种一维数组类
+
     }
 }
